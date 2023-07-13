bba0a0d5d32b89c8a9f690b348a1c108ef7c1ee6
Wolf <r.lanney@ics-nice.com>
2023-07-13 09:23:10 +0200

    tentative 0

--- src/components/ComponentFromJson.vue
+++ src/components/ComponentFromJson.vue
@@ -27,8 +27,8 @@
                     // Then specify how you want your dates to be formatted
                 return date.format("YYYY-MM-DDThh:mm");
             }, 
-            displayBlock(event){
-                console.log("test display", event.target.parentElement.nextSibling);
+            displayBlock(event:Event){
+                // console.log("test display", event.target.parentElement.nextSibling);
                 const valeur = event.target.innerText
                 if(valeur === "+"){
                     // on déplie 
@@ -39,7 +39,8 @@
                     event.target.innerText = "+"
                     event.target.parentElement.nextSibling.classList.remove("active")
                 }
-            }, deleteBloc(event){     
+            }, 
+            deleteBloc(event:Event){     
                 const bloc = event.target.parentElement.parentElement;
                 const blocs = bloc.parentElement.children.length;
 
@@ -64,21 +65,21 @@
   <div v-if="Object.keys(dataFromJson).length > 0">
     <div v-for="(elt, key) in dataFromJson" v-bind:key="key" >
         <!-- boucler pour afficher le formulaire en fonction du json  -->
-        <!-- {{ Array.isArray(elt) }} -->
-        <fieldset v-if="(typeof elt == 'object' && !Array.isArray(elt))">
+        <fieldset v-if="(typeof elt == 'object' && !Array.isArray(elt))" :class="key">
             <legend>{{ capitalized(key) }} <span class="plus" @click="displayBlock">+</span></legend>
             <div class="componentFromJson">
-                <ComponentFromJson :dataFromJson="elt" class="" />
+                <ComponentFromJson :dataFromJson="elt" :class="key" />
             </div> 
         </fieldset>
-        <div v-else-if="Array.isArray(elt)">
+        <div v-else-if="Array.isArray(elt)" class="array">
+            <!-- {{ Array.isArray(elt) }} -->
             <button class="btn" @click="elt.push(elt[0])">Ajouter un bloc</button>
             <fieldset v-for="(e, key) in elt" v-bind:key="key" class="blocArray">
                 <legend><button class="btn" @click="deleteBloc">Retirer ce bloc</button></legend>
                 <ComponentFromJson :dataFromJson="e" />
             </fieldset>
         </div>
-        <div v-else>
+        <div class="champs" v-else>
             <label :for="key">{{ capitalized(key) }}</label>
             <input 
                 :type="(typeof elt == 'string' && (key == 'horodatage' || key == 'dateInscription' || key == 'dateDebutExamen' || key == 'dateFinExamen' || key == 'dateDebutValidite' || key == 'dateFinValidite')) ? 'datetime-local' : typeof elt" 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import ComponentFromJson from "../components/ComponentFromJson.vue"
+import ComponentFromJson from "../components/ComponentFromJson.vue"
   
   export default {
     data() {
@@ -20,27 +20,45 @@
             })
     },
     methods:{
-      alert:(msg:string) => {
+      alert:(msg:any) => {
         //alert(msg);
         console.log(msg);
         
       },
-      generateXML: function(event:Event, elt:HTMLElement) {
-        let bloc;
-        this.alert("test")
-        if (typeof elt === 'undefined'){
-          bloc = document.querySelector("fieldset");
+      generateXML: function(event:Event, elt:HTMLFieldSetElement|undefined|null) {
+        let bloc:NodeListOf<HTMLFieldSetElement>|null = null;
+        //this.alert("generatexml");
+        if (elt === null){
+          this.alert("null");
+        } else if(typeof elt === 'undefined'){
+          bloc = document.querySelectorAll("fieldset");
+          // this.alert("undefined")
         } else {
-          bloc = elt.querySelector("fieldset");
+          bloc = elt.querySelectorAll("fieldset");
+          // this.alert("fieldset");
         }
 
+        if(bloc !== null){
+
+          // this.alert(bloc.filters)
+          for (const blocChild of bloc){
+            
+            //this.alert(inp)
+          }
+          const inp = bloc[0].querySelectorAll("div.componentFromJson > div."+bloc[0].className+" > div > div.champs > input");
+          //  this.alert(bloc)
+          this.alert(inp)
+          this.generateXML(event, bloc[0])
+        }
+
+        
       }
     } 
   }
 </script>
 
 <template>
-  <div>
+  <div id="form">
     <ComponentFromJson :dataFromJson="json" />
     <img src="@/assets/xml.png" alt="GenerateXML" id="GenerateXML" @click="generateXML">
   </div>
@@ -49,7 +67,7 @@
 <style scoped>
   #GenerateXML{
     width: 50px;
-    position: fixed;
+    position: fixed; 
     right: 2%;
     bottom: 2%;
   }
